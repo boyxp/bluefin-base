71e1b60e94ed6527fdfda4f1f9e98f60fb6dd85f
bluefin-framework <isme@live.com>
2016-02-13 22:58:34 +0800

    更改方法返回类型

--- src/registry/registry.php
+++ src/registry/registry.php
@@ -4,9 +4,9 @@ interface registry
 {
 	public function get(string $key);
 	public function __get(string $key);
-	public function set(string $key, $value, int $ttl=0);
-	public function __set(string $key, $value);
+	public function set(string $key, $value, int $ttl=0):registry;
+	public function __set(string $key, $value):registry;
 	public function exists(string $key):bool;
 	public function delete(string $key):bool;
-	public function flush():bool;
+	public function flush():registry;
 }
