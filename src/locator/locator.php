b094c1df79fe2a361f5b65f3e42a403434620111
bluefin-framework <isme@live.com>
2016-02-16 22:43:28 +0800

    新增locator接口

---
+++ src/locator/locator.php
@@ -0,0 +1,17 @@
+<?php
+namespace locator;
+interface locator
+{
+	public function get(string $service);
+	public function __get(string $service);
+
+	public function set(string $service, $instance):bool;
+	public function __set(string $service, $instance):bool;
+
+	public function make(string $service, array $args=null);
+	public function bind(string $service, array $args):closure;
+	public function alias(string $alias, string $service):bool;
+
+	public function has(string $service):bool;
+	public function remove(string $service):bool;
+}
