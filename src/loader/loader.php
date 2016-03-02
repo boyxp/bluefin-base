8fd02e417116a284d83e760903475a536620e7f0
boyxp <isme@live.com>
2016-03-02 22:10:21 +0800

    删除add方法

--- src/loader/loader.php
+++ src/loader/loader.php
@@ -2,7 +2,6 @@
 namespace loader;
 interface loader
 {
-	public function add(string $dir, bool $prepend=false):bool;
 	public function load(string $class):bool;
 	public function register(bool $prepend=false):bool;
 	public function unregister():bool;
