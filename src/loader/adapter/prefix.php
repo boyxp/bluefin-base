78b58698d600b4339cf20607529f30443a724f2d
boyxp <isme@live.com>
2016-03-03 23:12:02 +0800

    loader接口新增prefix实现，采用composer加载算法

---
+++ src/loader/adapter/prefix.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+namespace loader\adapter;
+use loader\loader as loaderInterface;
+class prefix implements loaderInterface
+{
+	private $_registered = false;
+	private $_prefixDirs = [];
+
+	public function add(string $prefix, string $dir):bool
+	{
+		if(!is_dir($dir)) {
+			return false;
+		}
+
+		if(!isset($this->_prefixDirs[$prefix{0}][$prefix])) {
+			$this->_prefixDirs[$prefix{0}][$prefix][$dir] = strlen($prefix)+1;
+			return true;
+
+		} elseif(!in_array($dir, $this->_prefixDirs[$prefix{0}][$prefix])) {
+			$this->_prefixDirs[$prefix{0}][$prefix][$dir] = strlen($prefix)+1;
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+
+	public function load(string $class):bool
+	{
+		if(!isset($this->_prefixDirs[$class{0}])) {
+			return false;
+		}
+
+		$path = strtr($class, '\\', DIRECTORY_SEPARATOR).'.php';
+
+		foreach($this->_prefixDirs[$class{0}] as $prefix => $dirs) {
+			if(strpos($class, $prefix)===0) {
+				foreach($dirs as $dir => $length) {
+					if(is_file($file=$dir.DIRECTORY_SEPARATOR.substr($path, $length))) {
+						include($file);
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public function register(bool $prepend=false):bool
+	{
+		if(!$this->_registered) {
+			$this->_registered = spl_autoload_register([$this, 'load'], true, $prepend);
+			return $this->_registered;
+		} else {
+			return false;
+		}
+	}
+
+	public function unregister():bool
+	{
+		if($this->_registered===false) {
+			return false;
+		} else {
+			return spl_autoload_unregister([$this, 'load']);
+		}
+	}
+}
