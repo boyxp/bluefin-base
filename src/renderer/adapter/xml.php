6f44bd63c24e8e9e68025bf137daa4fe21f8a0fe
boyxp <isme@live.com>
2016-03-09 21:35:22 +0800

    新增renderer接口xml实现

---
+++ src/renderer/adapter/xml.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+namespace bluefin\component\renderer\adapter;
+use XMLWriter;
+use bluefin\component\renderer\renderer as rendererInterface;
+class xml implements rendererInterface
+{
+	private $_charset = null;
+	private $_xml     = null;
+
+	public function __construct($charset='utf-8')
+	{
+		$this->_charset = $charset;
+	}
+
+	public function render($content):bool
+	{
+		header("Content-type: text/xml; charset={$this->_charset}");
+
+		$data = (!is_array($content) or count($content)>1) ? array('root'=>$content) : $content;
+
+		$this->_xml = new XMLWriter;
+		$this->_xml->openMemory();
+		$this->_xml->startDocument('1.0', $this->_charset);
+
+		$this->_build($data);
+
+		$this->_xml->endDocument();
+		echo $this->_xml->outputMemory(true);
+
+		return true;
+	}
+
+	private function _build($data, $node='node')
+	{
+		foreach($data as $key=>$value) {
+			$key = is_numeric($key) ? 'item' : $key;
+
+			$this->_xml->startElement($key);
+			if(is_array($value)) {
+				$this->{__FUNCTION__}($value, $key);
+			} else {
+				$this->_xml->text(strval($value));
+			}
+			$this->_xml->endElement();
+		}
+	}
+}
