cdeecf0bcdd7ab4790d049bc45e7a04f9631ed2f
boyxp <isme@live.com>
2016-03-10 22:02:13 +0800

    增加subject类型约束

--- src/router/router.php
+++ src/router/router.php
@@ -6,7 +6,7 @@ interface router
 	public function remove(string $pattern):router;
 	public function flush():router;
 
-	public function route($subject):bool;
+	public function route(string $subject):bool;
 	public function getHandle();
 	public function getMatches():array;
 }
