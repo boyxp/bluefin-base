ae5dd1f8938cd2620987496511ae5a74f897d5b4
boyxp <isme@live.com>
2016-03-19 21:35:01 +0800

    取消dispatch返回值约束

--- src/dispatcher/dispatcher.php
+++ src/dispatcher/dispatcher.php
@@ -2,7 +2,7 @@
 namespace bluefin\component\dispatcher;
 interface dispatcher
 {
-	public function dispatch($handle, array $params=[]):bool;
+	public function dispatch($handle, array $params=[]);
 	public function abort():bool;
 	public function forward($handle, array $params=[]):bool;
 }
