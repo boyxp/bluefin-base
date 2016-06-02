0d9f4d471a9abc67b850c5f5924728c38db4330f
boyxp <isme@live.com>
2016-06-02 19:53:15 +0800

    移除router、dispatcher、renderer构件

--- src/dispatcher/dispatcher.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace bluefin\component\dispatcher;
-interface dispatcher
-{
-	public function dispatch($handle, array $params=[]);
-	public function abort():bool;
-	public function forward($handle, array $params=[]):bool;
-}
--- src/renderer/renderer.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace bluefin\component\renderer;
-interface renderer
-{
-	public function render($content):bool;
-}
