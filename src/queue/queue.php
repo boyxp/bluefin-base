124d3f5989f5a8c3bc6161bf5dae900dea3f18ff
boyxp <isme@live.com>
2016-06-03 10:38:26 +0800

    移动cache和queue构件

--- src/queue/queue.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace bluefin\component\queue;
-interface queue
-{
-	public function enqueue(string $message):bool;
-	public function dequeue():string;
-	public function purge():bool;
-	public function delete():bool;
-}
